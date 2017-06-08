0d64ed0af09590cee66be0645eeb70bb4b075bec
Jason Lee <jason.x.lee@accenture.com>
2017-06-08 17:35:03 +1000

    test

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,13 +1,5 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
-
-import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsEvent,
-  LatLng,
-  CameraPosition
-} from '@ionic-native/google-maps';
+import { Component } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
 
 /**
  * Generated class for the MapPage page.
@@ -20,74 +12,6 @@ import {
   selector: 'page-map',
   templateUrl: 'map.html',
 })
-export class MapPage implements OnInit, AfterViewInit {
-  constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    private googleMaps: GoogleMaps,
-    public platform: Platform) {
-
-    platform.ready().then(() => {
-          this.loadMap();
-    });
-  }
-
-  ngOnInit() {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MapPage');
-  }
-
-  ngAfterViewInit() {
-     console.log('ngAfterViewInit MapPage');
-    // this.loadMap();
-  }
-
-  loadMap() {
-    console.log('loadMap MapPage');
-    // make sure to create following structure in your view.html file
-    // and add a height (for example 100%) to it, else the map won't be visible
-    // <ion-content>
-    //  <div #map id="map" style="height:100%;"></div>
-    // </ion-content>
-
-    // create a new map by passing HTMLElement
-    let element: HTMLElement = document.getElementById('map');
-
-    let map: GoogleMap = this.googleMaps.create(element);
-
-    // listen to MAP_READY event
-    // You must wait for this event to fire before adding something to the map or modifying it in anyway
-    map.one(GoogleMapsEvent.MAP_READY).then(
-      () => {
-        console.log('Map is ready!');
-        // Now you can add elements to the map like the marker
-      }
-    );
-
-    // create LatLng object
-    let ionic: LatLng = new LatLng(43.0741904, -89.3809802);
-
-    // create CameraPosition
-    let position: CameraPosition = {
-      target: ionic,
-      zoom: 18,
-      tilt: 30
-    };
-
-    // move the map's camera to position
-    map.moveCamera(position);
-
-    // create new marker
-    // let markerOptions: MarkerOptions = {
-    //   position: ionic,
-    //   title: 'Ionic'
-    // };
-
-    // const marker: Marker = map.addMarker(markerOptions)
-    //   .then((marker: Marker) => {
-    //     marker.showInfoWindow();
-    //   });
-  }
+export class MapPage{
+  constructor(public navCtrl: NavController) {}
 }
